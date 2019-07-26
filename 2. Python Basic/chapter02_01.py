3273bba7b425281b12126be8fe6b44d03ff36ce8
Si Hyeong Lee <super2451894@gmail.com>
2019-07-26 18:29:27 +0900

    Create chapter02_01.py
    - print
    - Learn about seperator option

---
+++ 2. Python Basic/chapter02_01.py
@@ -0,0 +1,17 @@
+# Python Basic
+
+print('Python Start')   # Most Used!!
+print("Python Start")
+print('''Python Start''')
+print("""Python Start""")
+
+print() # Empty Space
+
+# Separator Option
+print('P', 'Y', 'T', 'H', 'O', 'N', sep='')
+print('010', '7777', '1234', sep='-')
+print('Python', 'google.com', sep='@')
+
+print()
+
+# End Option
