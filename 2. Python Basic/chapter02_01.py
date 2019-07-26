fc624e32a750cb9e219db9d53b45a0acde2ebaef
Si Hyeong Lee <super2451894@gmail.com>
2019-07-26 18:44:14 +0900

    Update chapter02_01.py
    - Learn end option
    - Learn file option
    - Learn formating options

--- 2. Python Basic/chapter02_01.py
+++ 2. Python Basic/chapter02_01.py
@@ -15,3 +15,18 @@ print('Python', 'google.com', sep='@')
 print()
 
 # End Option
+print('Welcome to', end=' ')
+print('IT News', end=' ')
+print('Web Site')
+
+# File Option
+import sys
+print('Learn Python', file=sys.stdout)
+print()
+
+# Use Format (d : 3, s : 'python', f : 3.1444562)
+# s : String
+
+print('%s %s' % ('one', 'two'))
+print('{} {}'.format('one', 'two')) # Comfortable using method
+print('{1} {0}'.format('one', 'two'))
