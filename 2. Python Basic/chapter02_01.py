7d53ee8cfb480d6ab6181b491639a14ad58f46c2
Si Hyeong Lee <super2451894@gmail.com>
2019-07-29 23:09:03 +0900

    Update chapter02_01.py
    - Learn about print

--- 2. Python Basic/chapter02_01.py
+++ 2. Python Basic/chapter02_01.py
@@ -29,4 +29,33 @@ print()
 
 print('%s %s' % ('one', 'two'))
 print('{} {}'.format('one', 'two')) # Comfortable using method
-print('{1} {0}'.format('one', 'two'))
+print('{1} {0}'.format('one', 'two')) # Sequence
+
+print()
+
+# %s
+print('%10s' % ('nice'))
+print('{:>10}'.format('nice'))
+
+print('%-10s' % ('nice'))
+print('{:10}'.format('nice'))
+
+print('{:_>10}'.format('nice'))
+print('{:^>10}'.format('nice'))
+
+print('%.5s' % ('nice'))
+print('%.5s' % ('pythonstudy')) # cut
+print('{:10.5}'.format('pythonstudy'))
+
+# %d
+print('%d %d' % (1,2))
+print('{} {}'.format(1,2))
+
+print('%4d' % (42))
+print('{:4d}'.format(42))
+
+# %f
+print('%f' % (3.1494294242))
+print('{:f}'.format(3.1494294242))
+
+print('%06.2f' % (3.1494294242))
