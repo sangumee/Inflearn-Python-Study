f321299d3b01325602ab73ab2212e56d5538ddec
Si Hyeong Lee <super2451894@gmail.com>
2019-08-02 20:52:19 +0900

    File Write
    - Python File Write to TEXT

--- 9. Python File Write/1. chpater09_01.py
+++ 9. Python File Write/1. chpater09_01.py
@@ -19,3 +19,66 @@ print(cts)
 
 # Must be close
 f.close()
+
+# Ex2
+with open('./resource/it_news.txt', 'r', encoding='UTF-8') as f:
+    c = f.read()
+    print(c)
+    print(iter(c))
+    print(list(c))
+# Automatically close()
+print()
+
+# Ex3
+# read() : Read All, read(10) : 10Byte
+with open('./resource/it_news.txt', 'r', encoding='UTF-8') as f:
+    c = f.read(20)
+    print(c)
+    c = f.read(20)
+    print(c)
+    c = f.read(20)
+    print(c)
+    f.seek(0, 0)    # Go to First
+    c = f.read(20)
+    print(c)
+print()
+
+# Ex4
+# readline : read line by line
+with open('./resource/it_news.txt', 'r', encoding='UTF-8') as f:
+    line = f.readline()
+    print(line)
+    line = f.readline()
+    print(line)
+
+# Ex5
+# readlines : Read all and save with line with lists
+with open('./resource/it_news.txt', 'r', encoding='UTF-8') as f:
+    cts = f.readlines()
+    print(cts)
+    print()
+    for c in cts:
+        print(c, end='')
+print()
+
+# File Write
+
+# Ex1
+with open('./resource/contents1.txt', 'w') as f:
+    f.write('I love Python \n')
+
+# Ex2
+with open('./resource/contents1.txt', 'a') as f:
+    f.write('I love Python2 \n')
+
+# Ex3
+# writelines : list -> file
+with open('./resource/contents2.txt', 'w') as f:
+    list = ['Orange\n', 'Apple\n', 'Banana\n', 'Melon\n']
+    f.writelines(list)
+
+# Ex4
+with open('./resource/contents3.txt', 'w') as f:
+    print('Test Text Write!', file=f)
+    print('Test Text Write!', file=f)
+    print('Test Text Write!', file=f)
